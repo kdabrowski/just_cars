39cfac4e20f22423365924cff1110ebbd6fd5c5b
Krzysztof Dabrowski <krzysztof@toggl.com>
2019-11-17 18:52:32 +0100

    Add filtering

--- app/controllers/api/v1/adverts_controller.rb
+++ app/controllers/api/v1/adverts_controller.rb
@@ -1,5 +1,23 @@
 class Api::V1::AdvertsController < ApplicationController
   def index
-    render json: Advert.all
+    @adverts = Advert.where(nil)
+    filtering_params(filter_params).each do |key, value|
+      @adverts = @adverts.public_send(key, value) if value.present?
+    end
+    render json: @adverts
+  end
+
+  def show
+    render json: Advert.find(params[:id])
+  end
+
+  private
+
+  def filter_params
+    params.permit(:after, :before, :greater, :lower)
+  end
+
+  def filtering_params(params)
+    params.slice(:after, :before, :greater, :lower)
   end
 end
--- app/models/advert.rb
+++ app/models/advert.rb
@@ -1,3 +1,8 @@
 class Advert < ApplicationRecord
   has_one_attached :car_foto
+
+  scope :after, ->(date) { where('created_at >= ?', date) }
+  scope :before, ->(date) { where('created_at <= ?', date) }
+  scope :greater, ->(amount) { where('price >= ?', amount) }
+  scope :lower, ->(amount) { where('price <= ?', amount) }
 end
